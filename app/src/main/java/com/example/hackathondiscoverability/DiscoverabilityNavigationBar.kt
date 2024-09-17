24ab40aa5acf7e99c1b365abca74b5a0ce933d53
Irving Avila <iravilas@microsoft.com>
2024-09-17 10:59:43 -0700

    Few random things

--- app/src/main/java/com/example/hackathondiscoverability/DiscoverabilityNavigationBar.kt
+++ app/src/main/java/com/example/hackathondiscoverability/DiscoverabilityNavigationBar.kt
@@ -1,6 +1,21 @@
 package com.example.hackathondiscoverability
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.SizeTransform
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.keyframes
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -18,27 +33,32 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.primarySurface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.hackathondiscoverability.data.NavigationItem
 
 @Composable
-fun RowScope.NavigationMenuItem(
+fun NavigationMenuItem(
     title: String,
     iconRes: Int,
     isPromoted: Boolean = false
 ) {
     Box(
-        modifier = Modifier
-            .weight(2f),
         contentAlignment = Alignment.Center
     ) {
         Box {
@@ -71,12 +91,11 @@ fun RowScope.NavigationMenuItem(
 }
 
 @Composable
-fun RowScope.CompactNavigationMenuItem(
+fun CompactNavigationMenuItem(
     iconRes: Int,
 ) {
     Column(modifier = Modifier
-        .sizeIn(minWidth = 24.dp, minHeight = 56.dp)
-        .weight(1f),
+        .sizeIn(minWidth = 24.dp, minHeight = 56.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally) {
         Icon(painter = painterResource(id = iconRes), contentDescription = null)
@@ -102,14 +121,38 @@ fun DiscoverabilityNavigationBar(apps: List<NavigationItem>, promotedApp: Naviga
             verticalAlignment = Alignment.CenterVertically
         ) {
             for (app in apps) {
-                NavigationMenuItem(title = app.title, iconRes = app.iconRes)
+                Box(modifier = Modifier.weight(2f)) {
+                    NavigationMenuItem(title = app.title, iconRes = app.iconRes)
+                }
             }
 
-            if (promotedApp == null) {
-                NavigationMenuItem(title = "Apps", iconRes = R.drawable.ic_fluent_app_folder)
-            } else {
-                NavigationMenuItem(title = promotedApp.title, iconRes = promotedApp.iconRes, isPromoted = true)
-                CompactNavigationMenuItem(iconRes = R.drawable.ic_more_vertical_24_filled)
+            Box(Modifier.weight(if (promotedApp != null) 1f else 2f)) {
+                AnimatedContent(
+                    targetState = promotedApp != null,
+                    transitionSpec = {
+                        fadeIn(animationSpec = tween(150, 150)) togetherWith
+                            fadeOut(animationSpec = tween(150)) using
+                            SizeTransform { initialSize, targetSize ->
+                                if (targetState) {
+                                    keyframes {
+                                        IntSize(targetSize.width, initialSize.height) at 150
+                                        durationMillis = 300
+                                    }
+                                } else {
+                                    keyframes {
+                                        IntSize(initialSize.width, targetSize.height) at 150
+                                        durationMillis = 300
+                                    }
+                                }
+                            }
+                    }, label = "size transform"
+                ) { targetExpanded ->
+                    if (targetExpanded) {
+                        CompactNavigationMenuItem(iconRes = R.drawable.ic_more_vertical_24_filled)
+                    } else {
+                        NavigationMenuItem(title = "Apps", iconRes = R.drawable.ic_fluent_app_folder)
+                    }
+                }
             }
         }
     }
